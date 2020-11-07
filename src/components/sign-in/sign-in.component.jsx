64c59ce2a9f30187c6837e55550f24c81b381581
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-11-07 10:47:37 -0600

    removed email required error when signing in with Google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,7 +54,7 @@ class SignIn extends React.Component {
                         required />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                             {' '}
                             Sign in with Google{' '}
                         </CustomButton>
