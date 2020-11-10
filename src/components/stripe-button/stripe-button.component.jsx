979d8665d498962da32f4d7c5b7272412b954fee
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-11-09 21:07:27 -0600

    added stripe checkout

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HlmWZKlaZcgldaJFVAJIzJ76HKKCkNCxQzSBn3Rs6o51bFJjPEE0eacv2u4PEkpN7DCYDKCOk7sD1bm9poU9ilw00UpgrBjaL'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Crown Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton 
