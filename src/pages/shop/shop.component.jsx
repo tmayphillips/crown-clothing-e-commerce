e0d9fb53ff7a770a8da4217ea7fa77891bfb24e7
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-11-20 20:33:29 -0600

    create redux-saga for collections from firestore database

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,13 +3,12 @@ import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionPageContainer from '../collection/collection.container'
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
-import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors'
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
 class ShopPage extends React.Component {
     componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props
-        fetchCollectionsStartAsync()
+        const { fetchCollectionsStart } = this.props
+        fetchCollectionsStart()
     }
 
     render() {
@@ -33,7 +32,7 @@ class ShopPage extends React.Component {
 
 
 const mapDispatchToProps = (dispatch) => ({
-   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
+import createSagaMiddlewre from 'redux-saga'
 import rootReducer from './root-reducer'
-import thunk from 'redux-thunk'
+import rootSaga from './root-saga'
 
-const middlewares = [thunk]
+const sagaMiddleware = createSagaMiddlewre()
+const middlewares = [sagaMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+sagaMiddleware.run(rootSaga)
+
 export const persistor = persistStore(store)
