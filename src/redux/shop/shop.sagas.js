c5e4efd78dcd73f62ee104495f336085ab632086
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-11-24 17:48:29 -0600

    created cart and sign up sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
 import ShopActionTypes from './shop.types'
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions'
@@ -18,4 +18,10 @@ export function* fetchCollections() {
 
 export function* fetchCollectionsStart() {
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections)
+}
+
+export function* shopSagas() {
+    yield all([
+        call(fetchCollectionsStart)
+    ])
 }
