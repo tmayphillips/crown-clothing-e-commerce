7d46782d23c57373d86500ec5377eee22c3bb32f
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-12-20 19:47:43 -0600

    being mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
-import './App.css';
+
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import CheckoutPage from './pages/checkout/checkout.component'
+
+import  { GlobalStyle } from './global.styles'
+
 import { selectCurrentUser } from './redux/user/user.selectors'
 import { checkUserSession } from './redux/user/user.actions'
 import { createStructuredSelector } from 'reselect'
@@ -19,6 +22,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
